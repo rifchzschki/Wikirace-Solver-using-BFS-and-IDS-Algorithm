33de52ce50397747397ce9ad8afc370f0a9f6fc3
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2024-04-24 00:43:55 +0700

    bfs

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module Backend
 
 go 1.21.6
 
+require github.com/PuerkitoBio/goquery v1.9.1
+
 require (
-	github.com/PuerkitoBio/goquery v1.9.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	golang.org/x/net v0.24.0 // indirect
 )
--- src/Backend/BFS.go
+++ src/Backend/BFS.go
@@ -5,10 +5,53 @@ import (
 	"log"
 	"strings"
 	"time"
-	"github.com/gin-gonic/gin"
+	"os"
+	// "github.com/gin-gonic/gin"
 	"github.com/PuerkitoBio/goquery"
 )
 
+var unwantedWikiPrefixes = [...]string{"Wikipedia:", "Category:", "File:",
+	"Portal:", "Template:", "CS1_maint:", "Special:", "Template_talk:", "Help:", "Talk:"}
+
+func hasPrefix(names []string, str string) bool {
+	for _, name := range names {
+		if strings.HasPrefix(str, name) {
+			return true
+		}
+	}
+	return false
+}
+func isRedirect(doc *goquery.Document) bool {
+	return doc.Find("title").Text() == "Redirected" 
+}
+
+func scrapping(currentURL string)([]string){
+	queue := []string{}
+	visited := make(map[string]bool) // Map untuk menyimpan URL yang telah ditemukan
+
+	doc, err := goquery.NewDocument(currentURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if isRedirect(doc) {
+		return queue
+	}
+	
+	doc.Find("a[href]").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Attr("href")
+		if strings.HasPrefix(link, "/wiki/") && !hasPrefix(unwantedWikiPrefixes[:],link) && !strings.Contains(link,":"){
+			fullURL := "https://en.wikipedia.org" + link
+			
+			
+			if !visited[fullURL] {
+				fmt.Printf("Parent : %s, Child : %s\n", currentURL, fullURL)
+				queue = append(queue, fullURL)
+				visited[fullURL] = true 
+			}
+		}
+	})
+	return queue
+}
 func BFS(startURL, targetURL string) (string, string, string, time.Duration) {
 	startTime := time.Now()
 
@@ -18,49 +61,18 @@ func BFS(startURL, targetURL string) (string, string, string, time.Duration) {
 	articlesChecked := 0
 	articlesInSolution := 0
 
+	file, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
 	for len(queue) > 0 {
 		currentURL := queue[0]
 		queue = queue[1:]
+		found:
 
-		if visited[currentURL] {
-			continue
-		}
-
-		visited[currentURL] = true
-		articlesChecked++
-
-		doc, err := goquery.NewDocument(currentURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		foundTarget := false
-		neighbors := []string{}
-
-		doc.Find("#mw-content-text a").Each(func(i int, s *goquery.Selection) {
-			link, _ := s.Attr("href")
-			if strings.HasPrefix(link, "/wiki/") && !strings.Contains(link, ":") {
-				fullURL := "https://en.wikipedia.org" + link
-				neighbors = append(neighbors, fullURL)
-				if !visited[fullURL] && !foundTarget {
-					queue = append(queue, fullURL)
-					paths[fullURL] = currentURL
-				}
-				if fullURL == targetURL {
-					foundTarget = true
-				}
-			}
-		})
-
-		
-		for _, neighbor := range neighbors {
-			if !visited[neighbor] {
-				queue = append(queue, neighbor)
-			}
-		}
-
-		if foundTarget {
-			// Path found
+		if currentURL == targetURL{
 			path := []string{targetURL}
 			currentURL = targetURL
 			for currentURL != startURL {
@@ -69,35 +81,54 @@ func BFS(startURL, targetURL string) (string, string, string, time.Duration) {
 				articlesInSolution++
 			}
 			duration := time.Since(startTime)
+			
+
 			return "Shortest path: " + strings.Join(path, " -> "), fmt.Sprintf("Articles checked: %d", articlesChecked), fmt.Sprintf("Articles in solution: %d", articlesInSolution), duration
 		}
+		
+		if visited[currentURL] {
+			continue
+		}
+
+		visited[currentURL] = true
+		articlesChecked++
+
+		neighbors := scrapping(currentURL)
+		for _, neighbor := range neighbors {
+			if neighbor == targetURL{
+				paths[neighbor] = currentURL
+				currentURL = neighbor
+				goto found
+			}
+			if !visited[neighbor] {
+				queue = append(queue, neighbor)
+				
+				output := fmt.Sprintf("Parent : %s, Child : %s\n", currentURL, neighbor)
+				_, err := file.WriteString(output)
+				if err != nil {
+					log.Fatal("Cannot write to file", err)
+				}
+				
+				if _, ok := paths[neighbor]; !ok {
+					paths[neighbor] = currentURL
+				}
+				
+			}
+		}
 	}
+	
 
 	return "Path not found", "", "", 0
 }
 
+
+
 func main() {
-	r := gin.Default()
-
-	r.POST("/findpath", func(c *gin.Context) {
-		var input struct {
-			StartURL  string `json:"startURL"`
-			TargetURL string `json:"targetURL"`
-		}
-
-		if err := c.BindJSON(&input); err != nil {
-			c.JSON(400, gin.H{"error": "Invalid input"})
-			return
-		}
-
-		path, checked, inSolution, duration := BFS(input.StartURL, input.TargetURL)
-		c.JSON(200, gin.H{
-			"path":           path,
-			"checked":        checked,
-			"inSolution":     inSolution,
-			"timeTaken":      duration.String(),
-		})
-	})
-
-	r.Run(":8080")
+	startURL := "https://en.wikipedia.org/wiki/Tennis"
+	targetURL := "https://en.wikipedia.org/wiki/Stephen_Curry"
+	path, checked, inSolution, duration := BFS(startURL, targetURL)
+	fmt.Println(path)
+	fmt.Println(checked)
+	fmt.Println(inSolution)
+	fmt.Println(duration.String())
 }
