6ff5f8e9c95d60d1d2299a49f042782d88b732a0
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2024-04-25 22:55:04 +0700

    bfs mt

--- src/Backend/algorithm/BFSalgo.go
+++ src/Backend/algorithm/BFSalgo.go
@@ -1,17 +1,19 @@
 package algorithm
 
 import (
-	"fmt"
+	
 	"log"
-	"net/http"
 	"strings"
 	"time"
-
+	
+	"sync"
+	// "github.com/gin-gonic/gin"
 	"github.com/PuerkitoBio/goquery"
 )
 
 var unwantedWikiPrefixes = [...]string{"Wikipedia:", "Category:", "File:",
 	"Portal:", "Template:", "CS1_maint:", "Special:", "Template_talk:", "Help:", "Talk:"}
+
 
 func hasPrefix(names []string, str string) bool {
 	for _, name := range names {
@@ -27,57 +29,56 @@ func isRedirect(doc *goquery.Document) bool {
 
 func scrapping(currentURL string)([]string){
 	queue := []string{}
-	visited := make(map[string]bool)
+	exist := make(map[string]bool) 
 
-	resp, err := http.Get(currentURL)
-	if err != nil {
-		fmt.Printf("Error fetching URL %s: %v\n", currentURL, err)
-		return nil
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		fmt.Printf("Unexpected status code %d for URL %s\n", resp.StatusCode, currentURL)
-		return nil
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	doc, err := goquery.NewDocument(currentURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if isRedirect(doc) {
 		return queue
 	}
-	
-	doc.Find("a[href]").Each(func(i int, s *goquery.Selection) {
+	doc.Find("div#bodyContent").Each(func(i int, s *goquery.Selection) {
+		s.Find("a[href]").Each(func(i int, s *goquery.Selection) {
 		link, _ := s.Attr("href")
 		if strings.HasPrefix(link, "/wiki/") && !hasPrefix(unwantedWikiPrefixes[:],link) && !strings.Contains(link,":"){
 			fullURL := "https://en.wikipedia.org" + link
 			
 			
-			if !visited[fullURL] {
-				// fmt.Printf("Parent : %s, Child : %s\n", currentURL, fullURL)
+			if !exist[fullURL] {
 				queue = append(queue, fullURL)
-				visited[fullURL] = true 
+				exist[fullURL] = true 
 			}
 		}
-	})
+	})})
 	return queue
 }
-func BFS(startURL, targetURL string) ([] string, int, int, string) {
+
+
+const max = 50
+func BFS(startURL, targetURL string) ([]string, int,int,string) {
 	startTime := time.Now()
-
 	visited := make(map[string]bool)
-	queue := []string{startURL}
+	queue := make(chan string,100000000)  
 	paths := make(map[string]string)
-	articlesChecked := 0
+	articleChecked :=0
+	var mu sync.RWMutex
+	var wg sync.WaitGroup
+	ch := make(chan bool, max)
+	go func() {
+		queue <- startURL  
+	}()
+	
+	for currentURL := range queue {
+		articleChecked++
+		if time.Since(startTime) > (5 * time.Minute) {
 
-	for len(queue) > 0 {
-		currentURL := queue[0]
-		queue = queue[1:]
-		found:
-
-		if currentURL == targetURL{
+			return nil,articleChecked,0,time.Since(startTime).String()
+		}
+		here:
+		mu.Lock()
+		if currentURL == targetURL {
+			
 			path := []string{targetURL}
 			currentURL = targetURL
 			for currentURL != startURL {
@@ -85,36 +86,62 @@ func BFS(startURL, targetURL string) ([] string, int, int, string) {
 				path = append([]string{currentURL}, path...)
 			}
 			
-
-			return path, articlesChecked, len(path), time.Since(startTime).String()
+			return path,articleChecked, len(path),time.Since(startTime).String()
 		}
-		
+		mu.Unlock()
+
 		if visited[currentURL] {
 			continue
 		}
 
+		mu.Lock()
 		visited[currentURL] = true
-		articlesChecked++
+		mu.Unlock()
+		ch <- true
+		wg.Add(1)
+		
+		copyURL := currentURL 
 
-		neighbors := scrapping(currentURL)
-		for _, neighbor := range neighbors {
-			if neighbor == targetURL{
-				paths[neighbor] = currentURL
-				currentURL = neighbor
-				goto found
-			}
-			if !visited[neighbor] {
-				queue = append(queue, neighbor)
-				
-				if _, ok := paths[neighbor]; !ok {
-					paths[neighbor] = currentURL
+		go func(url string, targetURL string, paths *map[string]string) {
+			defer wg.Done()
+			defer func() { <-ch }()
+			neighbors := scrapping(url)
+			for _, neighbor := range neighbors {
+				mu.Lock()
+				if neighbor == targetURL {
+					if _, exist := (*paths)[neighbor]; !exist {
+						(*paths)[neighbor] = url
+					}
+					queue <- neighbor
+					copyURL = neighbor
+					mu.Unlock()
+					return
 				}
-				
-			}
-		}
-	}
-	
+				mu.Unlock()
 
-	return nil , articlesChecked, 0, time.Since(startTime).String()
+				if !visited[neighbor] {
+					mu.Lock()
+					if _, exist := (*paths)[neighbor]; !exist {
+						(*paths)[neighbor] = url
+					}
+					mu.Unlock()
+					queue <- neighbor
+				}
+			}
+		}(copyURL, targetURL, &paths)
+		wg.Wait()
+		mu.Lock()
+		if copyURL == targetURL{
+			currentURL = copyURL
+			mu.Unlock()
+			goto here
+		}
+		mu.Unlock()
+	}
+
+	wg.Wait()
+	close(queue)  
+	return nil, 0,0,time.Since(startTime).String()
 }
+
 
